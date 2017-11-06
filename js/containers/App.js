513107ac7e2ba832d09b95b9c6cb850af7ad1e10
John Hannagan <weetbix_@hotmail.com>
2017-11-06 23:08:15 +0100

    Change route generation to button click

--- js/containers/App.js
+++ js/containers/App.js
@@ -15,35 +15,29 @@ const style = {
 };
 
 class App extends React.Component {
-  componentDidMount() {
-    // For testing purposes, generate a new route every 5 seconds
-    setInterval(
-      () => {
-        const center = {lat: 52.4685913, long: 13.4309139};
-        const from = {
-          lat: center.lat + (Math.random() * 2 - 1) * 0.01,
-          long: center.long + (Math.random() * 2 - 1) * 0.01,
-        };
-        const to = {
-          lat: center.lat + (Math.random() * 2 - 1) * 0.01,
-          long: center.long + (Math.random() * 2 - 1) * 0.01,
-        };
-        this.props.genRoute(from, to);
-      },
-      50000,
-    );
-  }
-
   render() {
     return (
       <View style={style.container}>
         <NavMap coords={this.props.route.points} />
         <View style={{padding: 10}}>
-          <Button title="Go!" onPress={() => true} />
+          <Button title="Go!" onPress={() => this.generateRandomRoute()} />
         </View>
         <NextBarLabel name="Laika" distance={300} />
       </View>
     );
+  }
+
+  generateRandomRoute() {
+    const center = {lat: 52.4685913, long: 13.4309139};
+    const from = {
+      lat: center.lat + (Math.random() * 2 - 1) * 0.01,
+      long: center.long + (Math.random() * 2 - 1) * 0.01,
+    };
+    const to = {
+      lat: center.lat + (Math.random() * 2 - 1) * 0.01,
+      long: center.long + (Math.random() * 2 - 1) * 0.01,
+    };
+    this.props.genRoute(from, to);
   }
 }
 
