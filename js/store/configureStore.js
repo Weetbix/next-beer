87a5b61bd1a83397ac31b643e0b39992973bee9f
John Hannagan <weetbix_@hotmail.com>
2017-11-13 00:00:45 +0100

    Move state example to separate file and update it

--- js/store/configureStore.js
+++ js/store/configureStore.js
@@ -3,33 +3,6 @@ import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers';
 import {composeWithDevTools} from 'redux-devtools-extension';
 
-// store = {
-//     bar :
-//     {
-//         location : {}
-//         name :
-//         id
-//     },
-//     location :
-//     {
-// latitude :
-// longitute :
-//     }
-//     route : {
-//
-//         isFetching : false
-//         polyLine :
-//         distance :
-//         duration :
-//         name : barname
-//         destination : { coords }
-//     }
-//     previousBars : [
-//         Bar,
-//         Bar,
-//     ]
-// };
-
 export default function configureStore() {
   const store = createStore(
     rootReducer,
---
+++ js/store/storeExample.js
@@ -0,0 +1,17 @@
+store = {
+  location: {
+    latitude: 43.50,
+    longitute: 34.54,
+  },
+  route: {
+    isFetching: true / false,
+    polyLine: 'sc90a#8cj0a',
+    distance: '79 m',
+    duration: '1 min',
+    name: 'bar name',
+    destination: {
+      latitude: 43.5,
+      longitude: 23.5,
+    },
+  },
+};
