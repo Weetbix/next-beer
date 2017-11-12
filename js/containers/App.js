f9c5a4727d258a29a014519e69695d2486c79595
John Hannagan <weetbix_@hotmail.com>
2017-11-12 19:00:30 +0100

    Remove unused random geneneration function

--- js/containers/App.js
+++ js/containers/App.js
@@ -59,19 +59,6 @@ class App extends React.Component {
       </View>
     );
   }
-
-  generateRandomRoute() {
-    const center = {lat: 52.4685913, long: 13.4309139};
-    const from = {
-      lat: center.lat + (Math.random() * 2 - 1) * 0.01,
-      long: center.long + (Math.random() * 2 - 1) * 0.01,
-    };
-    const to = {
-      lat: center.lat + (Math.random() * 2 - 1) * 0.01,
-      long: center.long + (Math.random() * 2 - 1) * 0.01,
-    };
-    this.props.genRoute(from, to);
-  }
 }
 
 function mapStateToProps(state) {
