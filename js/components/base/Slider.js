e38eccdf88be4aa7831198bb30461f8c22edcd84
John Hannagan <weetbix_@hotmail.com>
2018-02-19 23:42:58 +0100

    Add ability to format display value

--- js/components/base/Slider.js
+++ js/components/base/Slider.js
@@ -11,15 +11,20 @@ const style = {
 
 export default class Slider extends React.Component {
   render() {
-    const {label} = this.props;
+    const {label, formatValue} = this.props;
 
     const sliderProps = {...this.props};
     delete sliderProps.label;
+    delete sliderProps.formatValue;
+
+    const displayValue = formatValue
+      ? formatValue(this.props.value)
+      : this.props.value;
 
     return (
       <View style={style.container}>
         <Text>
-          {`${label}: ${this.props.value}`}
+          {`${label}: ${displayValue}`}
         </Text>
         <NativeSlider
           {...sliderProps}
